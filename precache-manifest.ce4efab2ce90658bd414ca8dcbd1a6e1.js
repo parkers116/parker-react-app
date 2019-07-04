6fd857037eec2062fa85c92c851791493cab5960
parkers116 <parkers116@gmail.com>
2019-07-04 17:28:02 +0800

    Updates

---
+++ precache-manifest.ce4efab2ce90658bd414ca8dcbd1a6e1.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83f9b973481c06119cb4f08649632cc",
+    "url": "/parker-react-app/index.html"
+  },
+  {
+    "revision": "f6fb6fbab6f4d998a94e",
+    "url": "/parker-react-app/static/css/2.85c55ab2.chunk.css"
+  },
+  {
+    "revision": "a700a7f3eb7385890a41",
+    "url": "/parker-react-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f6fb6fbab6f4d998a94e",
+    "url": "/parker-react-app/static/js/2.727f88d2.chunk.js"
+  },
+  {
+    "revision": "a700a7f3eb7385890a41",
+    "url": "/parker-react-app/static/js/main.b1566b36.chunk.js"
+  },
+  {
+    "revision": "9d210d31d545ba9f9a13",
+    "url": "/parker-react-app/static/js/runtime~main.d7f5830d.js"
+  },
+  {
+    "revision": "d1b050ccc8ed3778833148566a58c480",
+    "url": "/parker-react-app/static/media/bk.d1b050cc.jpg"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/parker-react-app/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/parker-react-app/static/media/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/parker-react-app/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/parker-react-app/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/parker-react-app/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/parker-react-app/static/media/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "/parker-react-app/static/media/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/parker-react-app/static/media/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/parker-react-app/static/media/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/parker-react-app/static/media/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/parker-react-app/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/parker-react-app/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/parker-react-app/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/parker-react-app/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/parker-react-app/static/media/fa-solid-900.ef3df984.eot"
+  },
+  {
+    "revision": "ebafbc76d1222ba10a3d9dd7e1af4bc7",
+    "url": "/parker-react-app/static/media/icon.ebafbc76.svg"
+  }
+]);
